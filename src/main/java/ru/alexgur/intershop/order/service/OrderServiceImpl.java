1cdca1e398b11fc6b2fcaa99d7c97266310c1be3
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-05-30 14:46:33 +0300

    Remove unused import

--- src/main/java/ru/alexgur/intershop/order/service/OrderServiceImpl.java
+++ src/main/java/ru/alexgur/intershop/order/service/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.alexgur.intershop.order.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
