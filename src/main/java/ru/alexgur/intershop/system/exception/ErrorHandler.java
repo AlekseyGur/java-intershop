15c7d2cd943251dfb15db723547ff12b2f272810
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-06-23 16:04:19 +0300

    Change Error RestControllerAdvice responce type

--- src/main/java/ru/alexgur/intershop/system/exception/ErrorHandler.java
+++ src/main/java/ru/alexgur/intershop/system/exception/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.alexgur.intershop.system.exception;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,50 +15,65 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Mono<ErrorResponse> constraintViolationException(final ConstraintViolationException e) {
+    public Mono<ResponseEntity<ErrorResponse>> constraintViolationException(final ConstraintViolationException e) {
         log.error("Ошибка сервера. " + e.getMessage());
-        return Mono.just(new ErrorResponse(e.getMessage(), 400));
+
+        ErrorResponse error = new ErrorResponse(e.getMessage(), 400);
+        return Mono.just(ResponseEntity.badRequest().body(error));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Mono<ErrorResponse> methodArgumentNotValidException(final MethodArgumentNotValidException e) {
+    public Mono<ResponseEntity<ErrorResponse>> methodArgumentNotValidException(
+            final MethodArgumentNotValidException e) {
         log.error("Ошибка сервера. " + e.getMessage());
-        return Mono.just(new ErrorResponse(e.getMessage(), 400));
+
+        ErrorResponse error = new ErrorResponse(e.getMessage(), 400);
+        return Mono.just(ResponseEntity.badRequest().body(error));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Mono<ErrorResponse> internalServerException(final InternalServerException e) {
+    public Mono<ResponseEntity<ErrorResponse>> internalServerException(final InternalServerException e) {
         log.error("Ошибка сервера. " + e.getMessage());
-        return Mono.just(new ErrorResponse(e.getMessage(), 500));
+
+        ErrorResponse error = new ErrorResponse(e.getMessage(), 500);
+        return Mono.just(ResponseEntity.badRequest().body(error));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Mono<ErrorResponse> conditionsNotMetException(final ConditionsNotMetException e) {
+    public Mono<ResponseEntity<ErrorResponse>> conditionsNotMetException(final ConditionsNotMetException e) {
         log.error("Условия не выполнены. " + e.getMessage());
-        return Mono.just(new ErrorResponse(e.getMessage(), 500));
+
+        ErrorResponse error = new ErrorResponse(e.getMessage(), 500);
+        return Mono.just(ResponseEntity.badRequest().body(error));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public Mono<ErrorResponse> duplicatedDataException(final DuplicatedDataException e) {
+    public Mono<ResponseEntity<ErrorResponse>> duplicatedDataException(final DuplicatedDataException e) {
         log.error("Добавление данных. " + e.getMessage());
-        return Mono.just(new ErrorResponse(e.getMessage(), 409));
+
+        ErrorResponse error = new ErrorResponse(e.getMessage(), 409);
+        return Mono.just(ResponseEntity.badRequest().body(error));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Mono<ErrorResponse> notFoundException(final NotFoundException e) {
+    public Mono<ResponseEntity<ErrorResponse>> notFoundException(final NotFoundException e) {
         log.error("Ресурс не найден. " + e.getMessage());
-        return Mono.just(new ErrorResponse(e.getMessage(), 404));
+
+        ErrorResponse error = new ErrorResponse(e.getMessage(), 404);
+        return Mono.just(ResponseEntity.badRequest().body(error));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Mono<ErrorResponse> validationException(final ValidationException e) {
+    public Mono<ResponseEntity<ErrorResponse>> validationException(final ValidationException e) {
         log.error("Неверный запрос. " + e.getMessage());
-        return Mono.just(new ErrorResponse(e.getMessage(), 400));
+
+        ErrorResponse error = new ErrorResponse(e.getMessage(), 400);
+        return Mono.just(ResponseEntity.badRequest().body(error));
     }
 }
