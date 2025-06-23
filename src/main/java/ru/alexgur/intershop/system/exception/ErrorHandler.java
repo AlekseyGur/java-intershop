648cfbcd9bea758ab4427a2dd438d067fbad3063
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-06-23 15:58:14 +0300

    Change ErrorResponse fields

--- src/main/java/ru/alexgur/intershop/system/exception/ErrorHandler.java
+++ src/main/java/ru/alexgur/intershop/system/exception/ErrorHandler.java
@@ -14,50 +14,50 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Mono<ResponseEntity> constraintViolationException(final ConstraintViolationException e) {
+    public Mono<ErrorResponse> constraintViolationException(final ConstraintViolationException e) {
         log.error("Ошибка сервера. " + e.getMessage());
-        return Mono.just(new ResponseEntity(e.getMessage()));
+        return Mono.just(new ErrorResponse(e.getMessage(), 400));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Mono<ResponseEntity> methodArgumentNotValidException(final MethodArgumentNotValidException e) {
+    public Mono<ErrorResponse> methodArgumentNotValidException(final MethodArgumentNotValidException e) {
         log.error("Ошибка сервера. " + e.getMessage());
-        return Mono.just(new ResponseEntity(e.getMessage()));
+        return Mono.just(new ErrorResponse(e.getMessage(), 400));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Mono<ResponseEntity> internalServerException(final InternalServerException e) {
+    public Mono<ErrorResponse> internalServerException(final InternalServerException e) {
         log.error("Ошибка сервера. " + e.getMessage());
-        return Mono.just(new ResponseEntity(e.getMessage()));
+        return Mono.just(new ErrorResponse(e.getMessage(), 500));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Mono<ResponseEntity> conditionsNotMetException(final ConditionsNotMetException e) {
+    public Mono<ErrorResponse> conditionsNotMetException(final ConditionsNotMetException e) {
         log.error("Условия не выполнены. " + e.getMessage());
-        return Mono.just(new ResponseEntity(e.getMessage()));
+        return Mono.just(new ErrorResponse(e.getMessage(), 500));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public Mono<ResponseEntity> duplicatedDataException(final DuplicatedDataException e) {
+    public Mono<ErrorResponse> duplicatedDataException(final DuplicatedDataException e) {
         log.error("Добавление данных. " + e.getMessage());
-        return Mono.just(new ResponseEntity(e.getMessage()));
+        return Mono.just(new ErrorResponse(e.getMessage(), 409));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Mono<ResponseEntity> notFoundException(final NotFoundException e) {
+    public Mono<ErrorResponse> notFoundException(final NotFoundException e) {
         log.error("Ресурс не найден. " + e.getMessage());
-        return Mono.just(new ResponseEntity(e.getMessage()));
+        return Mono.just(new ErrorResponse(e.getMessage(), 404));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Mono<ResponseEntity> validationException(final ValidationException e) {
+    public Mono<ErrorResponse> validationException(final ValidationException e) {
         log.error("Неверный запрос. " + e.getMessage());
-        return Mono.just(new ResponseEntity(e.getMessage()));
+        return Mono.just(new ErrorResponse(e.getMessage(), 400));
     }
 }
