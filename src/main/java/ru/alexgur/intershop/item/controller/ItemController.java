e23369400e9e04f9296a160a58375638fbc4df1d
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-06-23 11:51:14 +0300

    Set status created for item add to cart

--- src/main/java/ru/alexgur/intershop/item/controller/ItemController.java
+++ src/main/java/ru/alexgur/intershop/item/controller/ItemController.java
@@ -2,6 +2,7 @@ package ru.alexgur.intershop.item.controller;
 
 import java.net.URI;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.reactive.result.view.Rendering;
 
@@ -42,6 +44,7 @@ public class ItemController {
     }
 
     @PostMapping("/items/create")
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<ServerResponse> createItem(@Valid @RequestBody Mono<ItemNewDto> item) {
         return itemService.add(item)
                 .then(ServerResponse.seeOther(URI.create("/"))
