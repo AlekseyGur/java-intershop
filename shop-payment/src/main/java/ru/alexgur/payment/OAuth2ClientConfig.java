a4fa9a4b42e0530e2c68fc7bd67b7cf25384cdad
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-07-14 12:08:32 +0300

    Add OAuth to payment service

---
+++ shop-payment/src/main/java/ru/alexgur/payment/OAuth2ClientConfig.java
@@ -0,0 +1,45 @@
+package ru.alexgur.payment;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OAuth2ClientConfig {
+
+    @Bean
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity security) throws Exception {
+        return security
+                .authorizeExchange(requests -> requests
+                        .pathMatchers(
+                                "/*",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/index.html")
+                        .permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(serverSpec -> serverSpec
+                        .jwt(jwtSpec -> {
+                            ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+                            jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+                                List<String> roles = jwt.getClaim("roles") == null ? List.of() : jwt.getClaim("roles");
+
+                                return Flux.fromIterable(roles)
+                                        .map(SimpleGrantedAuthority::new);
+                            });
+
+                            jwtSpec.jwtAuthenticationConverter(jwtAuthenticationConverter);
+                        }))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+}
